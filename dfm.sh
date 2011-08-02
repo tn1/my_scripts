f274fcd251aa5f48228d848936951e04226ebe97
t_n1 <tn1.linux@gmail.com>
2011-08-02 10:12:27 +0400

    fix error, warning.

--- dfm.sh
+++ dfm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-D_CONF="-fn -xos4-terminus-medium-r-normal--12-120-72-72-c-60-*-*  -nb #cccccc -nf #111111 -sb #111111 -sf #cccccc"
+D_CONF="-fn -xos4-terminus-medium-r-normal--12-120-72-72-c-60-*-* -nb #cccccc -nf #111111 -sb #111111 -sf #cccccc"
 DMENU="dmenu $D_CONF"
 C_DIR="$HOME/.config/dfm/"
 H_FILE="${C_DIR}dfm.last"
 M_FILE="${C_DIR}dfm.mime"
 
 is_type () {
-    if file --mime "$1" | grep $2 &> /dev/null
+    if file --mime "$1" | grep "$2" &> /dev/null
     then
 	return 0
     fi
@@ -15,17 +15,16 @@ is_type () {
 }
 
 cd_dir () {
-    cd $1
-    pwd > $H_FILE
+    cd "$1"
+    echo "`pwd`" > $H_FILE
 }
 
 open_file () {
-    echo "$1" "$2"
     eval "${1} \"${2}\"" &> /dev/null
 }
 
 run_cmd () {
-    cmd="echo Hello, ${USER}"
+    cmd="echo Hello, ${USER}!"
     while [ "$cmd" ] ; do
 	res=`eval "$cmd"`
 	cmd=`echo "$res" | $DMENU -p sh`
@@ -42,7 +41,7 @@ if [ -d "$C_DIR" ]; then
     if [ -e "$H_FILE" ]; then
 	y_n=`echo -e "Yes\nNo" | $DMENU -p "Open last dir?"`
 	if [ "$y_n" = "Yes" ]; then
-	    cd_dir `cat "$H_FILE"`
+	    cd_dir "`cat "$H_FILE"`"
 	elif [ "$y_n" = "" ]; then
 	    exit 0
 	fi
@@ -66,19 +65,19 @@ while true; do
     fi
 
     if [ -d "$var" ]; then
-	cd_dir $var
+	cd_dir "$var"
     elif [ "$var" = "shell:" ]; then
 	run_cmd
     elif [ "`echo $var | awk '{print $1}'`" = "sh:" ]; then
-	eval "`echo $var | sed s/sh://g`"
+	eval "`echo \"$var\" | cut -f2 -d:`"
     elif [ "$var" = "" ]; then
 	exit 0
     else
         echo "$M_FILE" | while read line
 	do
-	    if is_type "$var" `echo $line | awk '{print $1}'`
+	    if is_type "$var" `echo $line | cut -f1 -d:'`
 	    then
-		open_file `echo $line | awk '{print $2}' | tr "_" " "` "$var"
+		open_file "`echo $line | cut -f2 -d:`" "$var";
 		break
 	    fi
 	done
