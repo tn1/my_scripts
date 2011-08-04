cdcbc0306f151c33b7ef002c04dca337841c6801
t_n1 <tn1.linux@gmail.com>
2011-08-04 05:02:38 +0400

    Added make_mime ()

--- dfm.sh
+++ dfm.sh
@@ -7,7 +7,7 @@ H_FILE="${C_DIR}dfm.last"
 M_FILE="${C_DIR}dfm.mime"
 
 is_type () {
-    if file --mime "$1" | grep "$2" &> /dev/null
+    if file --mime "$1" | cut -f2 -d: | awk '{print $1}' | grep $2 &> /dev/null
     then
 	return 0
     fi
@@ -32,30 +32,59 @@ run_cmd () {
     return 0
 }
 
+run_program () {
+    while read line
+    do
+	if is_type "$2" `echo $line | cut -f1 -d:`
+	then
+	    open_file "`echo $line | cut -f2 -d:`" "$2"
+	    return 0
+	fi
+    done < $M_FILE
+    return 1
+}
+
+make_mime () {
+    new_mime=`file --mime "$1" | cut -f2 -d: | awk '{print $1}' | sed -e 's/;//g'`
+
+    if [ "$new_mime" ]; then
+	po=`echo "" | $DMENU -p "Enter program for $new_mime: "`
+	echo "$new_mime :$po" >> $M_FILE
+	return 0
+    fi
+    return 1
+}
+
 if [ -d "$C_DIR" ]; then
     if [ ! -e "$M_FILE" ]; then
-	echo Exit | `$DMENU -p "Create $M_FILE. Format: mime program"`
-	exit 1
+	y_n=`echo -e "Yes\nNo" | $DMENU -p "Create $M_FILE. Format: mime program. Exit?"`
+	if [ Yes = "$y_n" ];then
+	    exit 0
+	fi
+	y_n=""
     fi
 
     if [ -e "$H_FILE" ]; then
 	y_n=`echo -e "Yes\nNo" | $DMENU -p "Open last dir?"`
-	if [ "$y_n" = "Yes" ]; then
+	if [ Yes = "$y_n" ]; then
 	    cd_dir "`cat "$H_FILE"`"
-	elif [ "$y_n" = "" ]; then
+	elif [ "" = "$y_n" ]; then
 	    exit 0
 	fi
+	y_n=""
     fi
 elif [ ! -d "$C_DIR" ]; then
-    mkdir $C_DIR
-    echo Exit | `$DMENU -p "Create $M_FILE. Format: mime program"`
-    exit 1
+    mkdir -p "$C_DIR"
+    y_n=`echo -e "Yes\nNo" | $DMENU -p "Create $M_FILE. Format: mime program. Exit?"`
+    if [ Yes = "$y_n" ];then
+	exit 0
+    fi
+    y_n=""
 else
     return 0
 fi
 
 var=`ls`
-M_FILE=`cat $M_FILE`
 
 while true; do
     if [ "$var" ]; then
@@ -66,21 +95,17 @@ while true; do
 
     if [ -d "$var" ]; then
 	cd_dir "$var"
-    elif [ "$var" = "shell:" ]; then
+    elif run_program "$M_FILE" "$var"
+    then
+	return 0
+    elif [ shell: = "$var"  ]; then
 	run_cmd
     elif [ "`echo $var | awk '{print $1}'`" = "sh:" ]; then
 	eval "`echo \"$var\" | cut -f2 -d:`"
-    elif [ "$var" = "" ]; then
+    elif [ x"$var" = x"" ]; then
 	exit 0
     else
-	echo "$M_FILE" | while read line
-	do
-	    if is_type "$var" `echo $line | cut -f1 -d:`
-	    then
-		open_file "`echo $line | cut -f2 -d:`" "$var";
-		break
-	    fi
-	done
+	make_mime "$var" || exit 1
     fi
 
     var=`ls`
